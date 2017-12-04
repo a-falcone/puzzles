e7adb368130a9f237d8fadc87421d5f22735177d
Adrian Falcone <falcone@gmail.com>
2017-12-04 06:42:56 -0500

    Adding python solutions for 03

---
+++ adventofcode/2017/03a.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+"""
+--- Day 3: Spiral Memory ---
+
+You come across an experimental new kind of memory stored on an infinite two-dimensional grid.
+
+Each square on the grid is allocated in a spiral pattern starting at a location marked 1 and then counting up while spiraling outward. For example, the first few squares are allocated like this:
+
+        17  16  15  14  13
+        18   5   4   3  12
+        19   6   1   2  11
+        20   7   8   9  10
+        21  22  23---> ...
+        While this is very space-efficient (no squares are skipped), requested data must be carried back to square 1 (the location of the only access port for this memory system) by programs that can only move up, down, left, or right. They always take the shortest path: the Manhattan Distance between the location of the data and square 1.
+
+        For example:
+
+        Data from square 1 is carried 0 steps, since it's at the access port.
+        Data from square 12 is carried 3 steps, such as: down, left, left.
+        Data from square 23 is carried only 2 steps: up twice.
+        Data from square 1024 must be carried 31 steps.
+
+How many steps are required to carry the data from the square identified in your puzzle input all the way to the access port?
+
+Your puzzle input is 277678.
+"""
+
+import math
+
+data = [1, 12, 23, 1024, 277678]
+
+for d in data:
+  loc = [0, 0]
+  maxx = 0
+  minx = 0
+  maxy = 0
+  miny = 0
+  dir = 'r'
+  i = 1
+  while (i < d):
+    i += 1
+    if dir == 'r':
+      loc[0] += 1
+      if loc[0] > maxx:
+        dir = 'u'
+        maxx += 1
+        continue
+    if dir == 'u':
+      loc[1] += 1
+      if loc[1] > maxy:
+        dir = 'l'
+        maxy += 1
+        continue
+    if dir == 'l':
+      loc[0] -= 1
+      if loc[0] < minx:
+        dir = 'd'
+        minx -= 1
+        continue
+    if dir == 'd':
+      loc[1] -= 1
+      if loc[1] < miny:
+        dir = 'r'
+        miny -= 1
+        continue
+  print d, abs(loc[0]) + abs(loc[1])
