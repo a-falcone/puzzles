a12354379911fc4c3013a74f44583863cb66083d
Adrian Falcone <falcone@gmail.com>
2019-12-04 22:29:44 -0500

    Adding day 4

---
+++ adventofcode/2019/04b.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+"""
+You arrive at the Venus fuel depot only to discover it's protected by a password. The Elves had written the password on a sticky note, but someone threw it out.
+
+However, they do remember a few key facts about the password:
+
+It is a six-digit number.
+The value is within the range given in your puzzle input.
+Two adjacent digits are the same (like 22 in 122345).
+Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
+Other than the range rule, the following are true:
+
+111111 meets these criteria (double 11, never decreases).
+223450 does not meet these criteria (decreasing pair of digits 50).
+123789 does not meet these criteria (no double).
+How many different passwords within the range given in your puzzle input meet these criteria?
+
+--- Part Two ---
+An Elf just remembered one more important detail: the two adjacent matching digits are not part of a larger group of matching digits.
+
+Given this additional criterion, but still ignoring the range rule, the following are now true:
+
+112233 meets these criteria because the digits never decrease and all repeated digits are exactly two digits long.
+123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444).
+111122 meets the criteria (even though 1 is repeated more than twice, it still contains a double 22).
+How many different passwords within the range given in your puzzle input meet all of the criteria?
+"""
+
+import re
+
+c = 0
+for i in range(265275,781584+1):
+  matches = False
+  s = str(i)
+  if re.search(r"^1*2*3*4*5*6*7*8*9*$", s):
+    groups = re.findall(r"(.)\1", s)
+    for g in groups:
+      if not re.search(re.escape(g) + r"{3}", s):
+        matches = True
+  if matches:
+    c += 1
+
+print(c)
