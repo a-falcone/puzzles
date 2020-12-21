388547234ab615afbf409c6162bce82f35e04e4d
Adrian Falcone <falcone@gmail.com>
2020-12-21 10:12:27 -0500

    Solution for 21

--- adventofcode/2020/21b.py
+++ adventofcode/2020/21b.py
@@ -21,6 +21,18 @@ The first food in the list has four ingredients (written in a language you don't
 The first step is to determine which ingredients can't possibly contain any of the allergens in any food in your list. In the above example, none of the ingredients kfcds, nhms, sbzzf, or trh can contain an allergen. Counting the number of times any of these ingredients appear in any ingredients list produces 5: they all appear once each except sbzzf, which appears twice.
 
 Determine which ingredients cannot possibly contain any of the allergens in your list. How many times do any of those ingredients appear?
+
+--- Part Two ---
+Now that you've isolated the inert ingredients, you should have enough information to figure out which ingredient contains which allergen.
+
+In the above example:
+
+mxmxvkd contains dairy.
+sqjhc contains fish.
+fvjkl contains soy.
+Arrange the ingredients alphabetically by their allergen and separate them by commas to produce your canonical dangerous ingredient list. (There should not be any spaces in your canonical dangerous ingredient list.) In the above example, this would be mxmxvkd,sqjhc,fvjkl.
+
+Time to stock your raft with supplies. What is your canonical dangerous ingredient list?
 """
 
 import re
@@ -45,18 +57,17 @@ for line in data:
             else:
                 foods[f] = foods[f].intersection( ings )
 
-for i in ingredients.copy():
-    keep = True
-    for v in foods.values():
-        if i in v:
-            keep = False
-    if not keep:
-        ingredients.remove( i )
+changed = True
+while changed:
+    changed = False
+    for i in foods:
+        if len(foods[i]) == 1:
+            a = list(foods[i])[0]
+            for j in foods:
+                if len(foods[j]) == 1:
+                    continue
+                if a in foods[j]:
+                    changed = True
+                    foods[j].remove(a)
 
-count = 0
-for i in ingredients:
-    for line in data:
-        m = re.fullmatch( "(.*) \(contains (.*)\)", line )
-        if i in m.group(1).split(" "):
-            count += 1
-print( count )
+print( ",".join( [foods[f].pop() for f in sorted(foods.keys())] ) )
