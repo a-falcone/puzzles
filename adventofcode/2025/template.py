073b1673eb8d78e871395ba6ea0a4591eeaf04d3
Adrian Falcone <falcone@gmail.com>
2025-02-05 08:24:37 -0500

    Adding template to 2025

---
+++ adventofcode/2025/template.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def load_data(filename: str) -> list:
+    data = []
+    with open(filename, "r") as f:
+        for line in f:
+            data.append(line.rstrip())
+    return data
+
+if __name__ == "__main__":
+    data = load_data("CHANGEME.data")
+    data = load_data("CHANGEME.test")
+
+"""
+"""
