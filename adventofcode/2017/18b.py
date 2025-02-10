ea791224dc7df4e11dabc8616011b9bbe61178c7
Adrian Falcone <falcone@gmail.com>
2025-02-10 10:53:43 -0500

    2027 Day 18 and template

--- adventofcode/2017/18b.py
+++ adventofcode/2017/18b.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 class Computer():
-    def __init__(self, instructions):
-        self.register = {}
+    def __init__(self, instructions, p):
+        self.register = {'p': p}
         self.instructions = instructions
         self.pointer = 0
-        self.last_sound = 0
+        self.queue = []
+        self.is_dead = False
 
     def value(self, r):
         if type(r) == int:
@@ -14,18 +15,22 @@ class Computer():
             self.register[r] = 0
         return self.register[r]
 
-    def run(self):
+    def run(self, queue):
+        if self.is_dead:
+            return
+        self.to_send = []
+        self.queue.extend(queue)
         while True:
             if self.pointer < 0 or self.pointer >= len(self.instructions):
-                print(f'Pointer = {self.pointer}. Terminating.')
-                return
+                self.is_dead = True
+                return []
 
             instruction, args = self.instructions[self.pointer]
             if args[0] not in self.register:
                 self.register[args[0]] = 0
 
             if instruction == 'snd':
-                self.last_sound = self.value(args[0])
+                self.to_send.append(self.value(args[0]))
             elif instruction == 'set':
                 self.register[args[0]] = self.value(args[1])
             elif instruction == 'add':
@@ -35,8 +40,9 @@ class Computer():
             elif instruction == 'mod':
                 self.register[args[0]] %= self.value(args[1])
             elif instruction == 'rcv':
-                if self.value(args[0]) != 0:
-                    return(self.last_sound)
+                if len(self.queue) == 0:
+                    return self.to_send
+                self.register[args[0]] = self.queue.pop(0)
             elif instruction == 'jgz':
                 if self.value(args[0]) > 0:
                     self.pointer += self.value(args[1])
@@ -56,10 +62,25 @@ def load_data(filename: str) -> list:
 
 if __name__ == "__main__":
     instructions = load_data("18.data")
-    #instructions = load_data("18.test")
 
-    c = Computer(instructions)
-    print(c.run())
+    c0 = Computer(instructions, 0)
+    c1 = Computer(instructions, 1)
+    count = 0
+    q1 = []
+
+    while True:
+        q0 = c0.run(q1)
+        q1 = c1.run(q0)
+
+        count += len(q1)
+
+        if c0.is_dead and c1.is_dead:
+            break
+
+        if len(q1) == 0:
+            break
+    
+    print(count)
 
 """
 --- Day 18: Duet ---
@@ -99,4 +120,30 @@ Finally, a is set to 1, causing the next jgz instruction to activate, jumping ba
 At the time the recover operation is executed, the frequency of the last sound played is 4.
 
 What is the value of the recovered frequency (the value of the most recently played sound) the first time a rcv instruction is executed with a non-zero value?
+
+--- Part Two ---
+As you congratulate yourself for a job well done, you notice that the documentation has been on the back of the tablet this entire time. While you actually got most of the instructions correct, there are a few key differences. This assembly code isn't about sound at all - it's meant to be run twice at the same time.
+
+Each running copy of the program has its own set of registers and follows the code independently - in fact, the programs don't even necessarily run at the same speed. To coordinate, they use the send (snd) and receive (rcv) instructions:
+
+snd X sends the value of X to the other program. These values wait in a queue until that program is ready to receive them. Each program has its own message queue, so a program can never receive a message it sent.
+rcv X receives the next value and stores it in register X. If no values are in the queue, the program waits for a value to be sent to it. Programs do not continue to the next instruction until they have received a value. Values are received in the order they are sent.
+Each program also has its own program ID (one 0 and the other 1); the register p should begin with this value.
+
+For example:
+
+snd 1
+snd 2
+snd p
+rcv a
+rcv b
+rcv c
+rcv d
+Both programs begin by sending three values to the other. Program 0 sends 1, 2, 0; program 1 sends 1, 2, 1. Then, each program receives a value (both 1) and stores it in a, receives another value (both 2) and stores it in b, and then each receives the program ID of the other program (program 0 receives 1; program 1 receives 0) and stores it in c. Each program now sees a different value in its own copy of register c.
+
+Finally, both programs try to rcv a fourth time, but no data is waiting for either of them, and they reach a deadlock. When this happens, both programs terminate.
+
+It should be noted that it would be equally valid for the programs to run at different speeds; for example, program 0 might have sent all three values and then stopped at the first rcv before program 1 executed even its first instruction.
+
+Once both of your programs have terminated (regardless of what caused them to do so), how many times did program 1 send a value?
 """
